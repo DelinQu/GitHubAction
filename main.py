ce26d7efb287679d920a327a746219a49c008682
Delin Qu <qdl.cs@qq.com>
2021-10-30 17:27:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+import sys, getopt
+
 asciiText = ""\
     "##   ##  #######    ###    ##     ######  ##   ##            #####   ##   ##  #######   #####   ##   ##  \n" \
     "##   ##  ##        ## ##   ##       ##    ##   ##           ##   ##  ##   ##  ##       ##   ##  ##  ##   \n" \
@@ -7,6 +10,28 @@ asciiText = ""\
     "##   ##  ##       ##   ##  ##       ##    ##   ##           ##   ##  ##   ##  ##       ##   ##  ##  ##   \n" \
     "##   ##  #######  ##   ##  #######  ##    ##   ##            #####   ##   ##  #######   #####   ##   ##  \n"
 
+def getArg(argv):
+   username = ''
+   passwd = ''
+   email = ''
 
-if __name__ == '__main__':
+   try:
+       opts, args = getopt.getopt(argv,"hu:p:e:")
+   except getopt.GetoptError:
+      print('main.py -u <username> -p <passwd> -e <email>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('main.py -u <username> -p <passwd> -e <email>')
+         sys.exit()
+      elif opt == "-u":
+         username = arg
+      elif opt == "-p":
+         passwd = arg
+      elif opt == "-e":
+         email = arg
+   return username,passwd,email
+
+if __name__ == "__main__":
     print(asciiText)
+    print(getArg())
